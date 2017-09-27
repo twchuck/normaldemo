691a56a928c013c158fbab40d3011cf46180945e
twchuck1 <32159827+twchuck@users.noreply.github.com>
2017-09-27 15:38:25 +0800

    add normal calculator

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -3,18 +3,18 @@ package main;
 public class Calculator {
 
 	public static int add(int a, int b) {
-		return a + b + b + b;
+		return a + b;
 	}
 
 	public static int sub(int a, int b) {
-		return a - b - b - b - b;
+		return a - b;
 	}
 
 	public static int multiply(int a, int b) {
-		return a * b * b;
+		return a * b;
 	}
 
 	public static int div(int a, int b) {
-		return a / b / b;
+		return a / b;
 	}
 }
---
+++ src/main/NormalCalculator.java
@@ -0,0 +1,8 @@
+package main;
+
+public class NormalCalculator {
+
+	public static int add(int a, int b) {
+		return a + b + b + b;
+	}	
+}
